1e30dea459e387cd50fc844823ccdd0afde7d593
pavel jašek <paveljasek@gmail.com>
2014-11-23 23:40:59 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 getting-cleaning-data
 =====================
 
-course project
+Course project for Getting and Cleaning Data on Coursera.
+
+Description of each file:
+
+- CodeBook.md describes the variables, the data, and any transformations or work that you performed to clean up the data
+- run_analysis.R 
+    1. Merge the training and the test sets to create one data set.
+    2. Extract only the measurements on the mean and standard deviation for each measurement. 
+    3. Use descriptive activity names to name the activities in the data set
+    4. Appropriately label the data set with descriptive variable names. 
+    5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+    6. Write data output
---
+++ run_analysis.R
@@ -0,0 +1,30 @@
+# Getting and Cleaning Data Course ProjectHelp
+# Pavel Jasek, paveljasek@gmail.com
+
+# this script:
+# Merges the training and the test sets to create one data set.
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
+# Uses descriptive activity names to name the activities in the data set
+# Appropriately labels the data set with descriptive variable names. 
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+
+
+
+# 0. load required libraries -----------------------------
+
+# 1. Merge the training and the test sets to create one data set. -----------------------------
+
+# 2. Extract only the measurements on the mean and standard deviation for each measurement. -----------------------------
+
+# 3. Use descriptive activity names to name the activities in the data set -----------------------------
+
+# 4. Appropriately label the data set with descriptive variable names. -----------------------------
+
+# 5. Average of each variable for each activity and each subject -----------------------------
+# From the data set in step 4, creates a second, independent tidy data set
+# with the average of each variable for each activity and each subject.
+
+# 6. Write data output
+# write.table() using row.name=FALSE
